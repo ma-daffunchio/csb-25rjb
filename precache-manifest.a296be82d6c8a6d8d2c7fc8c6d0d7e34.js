2bbeac8c39f07fe57498fc5087a34513c28d7d96
Code Sandbox <hello@codesandbox.io>
2022-01-27 18:42:37 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a296be82d6c8a6d8d2c7fc8c6d0d7e34.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "219f573978a137184bc6",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/css/main.6bef4535.chunk.css"
+  },
+  {
+    "revision": "219f573978a137184bc6",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/js/main.219f5739.chunk.js"
+  },
+  {
+    "revision": "3d474ff57e27c85cdda6",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/css/1.d7fba431.chunk.css"
+  },
+  {
+    "revision": "3d474ff57e27c85cdda6",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/js/1.3d474ff5.chunk.js"
+  },
+  {
+    "revision": "b367edd3587928ef30fe",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/js/runtime~main.b367edd3.js"
+  },
+  {
+    "revision": "ef3c4c2ed70a5df5f652c81f700cf977",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/cover-img.ef3c4c2e.jpg"
+  },
+  {
+    "revision": "eaafe86cb500e379277cce23ea5e3f04",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/shuttershare-hero.eaafe86c.jpg"
+  },
+  {
+    "revision": "5909a3daef3b75e0f6b3f90d60063622",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/canada.5909a3da.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "1d92a789d863b5e62427861a49c76245",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/adscope-hero.1d92a789.jpg"
+  },
+  {
+    "revision": "5e7ad168af891bdb24d8df2c333851ab",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/tiptopmusic-hero.5e7ad168.jpg"
+  },
+  {
+    "revision": "2cfcf00f4a8544a4c7b34f1ade88ffc0",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/schoolforjustice-hero.2cfcf00f.jpg"
+  },
+  {
+    "revision": "1c096329cfe94feadbd93a82f6c69f93",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/static/media/damtotdamloop-hero.1c096329.jpg"
+  },
+  {
+    "revision": "11198bb441c78097fd04d7f7242b0667",
+    "url": "https://ma-daffunchio.github.io/csb-25rjb/index.html"
+  }
+];
